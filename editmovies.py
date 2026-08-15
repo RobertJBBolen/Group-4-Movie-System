96bb301f87f8c248b9ad6abbd0a0156cf7397f7e
RobertJBBolen <rjbaltacc@gmail.com>
2026-08-15 06:33:01 +0000

    Added Delete function

--- editmovies.py
+++ editmovies.py
@@ -5,9 +5,10 @@ movie_genres = {"Action": ["Spiderman Brand new day"],
 }
 
 def show_Movies():
-    print("=" * 30)
+    print("=" * 50)
     for genre, movies in movie_genres.items():
         print(f"Genre: {genre} Movie: {movies}")
+    print("=" * 50)
 
 show_Movies()
 
@@ -16,7 +17,18 @@ def update_movies(genre, oldTitle, newTitle):
         if movies == oldTitle:
             movie_genres[genre][i] = newTitle
 
+def delete_movies(genre, title):
+    if genre in movie_genres:
+        if title in movie_genres[genre]:
+            movie_genres[genre].remove(title)
 
 update_movies("Action", "Spiderman Brand new day", "Spiderman")
+update_movies("Musical", "Mamma Mia", "The Greatest Showman")
 
 print(movie_genres)
+
+delete_movies("Sci-Fi", "Star wars")
+
+print(movie_genres)
+
+show_Movies()
