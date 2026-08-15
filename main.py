5f8782ad1d9df12264b298260098ffc3e40db13d
Sigua, Kenneth C. <kennethsigua781@gmail.com>
2026-08-15 06:29:35 +0000

    intro display

--- main.py
+++ main.py
@@ -7,3 +7,14 @@
 # Review/Comments
 
 # 
+
+
+def display():
+    print("Welcome to CineCore, your ultimate command-line hub for exploring, reviewing, and tracking your favorite films!")
+    userinfo()
+    
+def userinfo():
+    username = input("Enter username for your account:")
+    favorite_Genre = input("What your Favorite Genre:")
+
+display()
