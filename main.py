516e6a8cfa5f7771ce307fe1d4382cb3c785d7a3
peredachris14 <chriseukipereda@gmail.com>
2026-08-16 06:26:47 +0000

    main.py

--- main.py
+++ main.py
@@ -9,3 +9,21 @@
 # UPDATE / (Updating Movie titles & managing movies.)
 
 # DELETE / (Delete movies)
+
+def search():
+    print("\nSEARCH MOVIES\n")
+
+    keyword = input("Enter movie title or genre: ").strip().lower()
+
+    found = False
+
+    for movie in movies:
+        if keyword in movie["title"].lower() or keyword in movie["genre"].lower():
+            print(f"\nTitle: {movie['title']}")
+            print(f"Genre: {movie['genre']}")
+            found = True
+
+    if not found:
+        print("\nNo movies found.")
+
+    input("\nPress Enter to return to main menu...")
