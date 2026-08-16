68dd7af6a8d58beb9180702c0744ad4f6825703e
Sigua, Kenneth C. <kennethsigua781@gmail.com>
2026-08-16 04:20:28 +0000

    Added menu function

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 genres = ["Action", "Comedy", "Drama", "Horror", "Sci-Fi", "Fantasy", "Romance", "Thriller"]
+width = 60
 
 def banner():
-    width = 60
 
     print("="*width+"\n")
     print(" "+"🎬 WELCOME 🎬".center(width)+" \n")
@@ -36,4 +36,39 @@ def register():
     print(genres)
     favGenre = input("Enter your Favorite Genre: ")
 
-banner()
+def menu():
+    while True:
+        print()
+        print("="*width)
+        print(""+"MAIN MENU".center(width)+"")
+        print("="*width)
+        print("\n [1] Suggested Movies\n [2] Search Movies\n [3] Add Movie\n [4] Update Movie\n [5] Delete Movie\n [6] Profile\n [7] Exit\n  ")
+        print("="*width)
+
+        choice = int(input("Enter your choice: "))
+            
+        if choice == 1:
+            print("\nOpening suggested movie...")
+            suggested()
+        elif choice == 2:
+            print("\nOpening search movie...")
+            search()
+        elif choice == 3:
+            print("\nOpening add movie...")
+            addMovie()
+        elif choice == 4:
+            print("\nOpening update movie...")
+            updateMovie()
+        elif choice == 5:
+            print("\nOpening delete movie...")
+            deleteMovie()
+        elif choice == 6:
+            print("\nOpening profile...")
+            profile(user)
+        elif choice == 7:
+            print("\nThank you for using screenspace")
+            break
+        else:
+            print("\nInvalid choice")
+    
+menu()
