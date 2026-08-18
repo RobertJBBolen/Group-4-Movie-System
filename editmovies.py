bdb03c8276776167923548c64193726c177a817d
RobertJBBolen <rjbaltacc@gmail.com>
2026-08-18 22:23:08 +0800

    Updated Update and Delete

--- editmovies.py
+++ editmovies.py
@@ -13,23 +13,37 @@ def show_Movies():
 show_Movies()
 
 def update_movies(genre, oldTitle, newTitle):
-    for i, movies in enumerate(movie_genres[genre]):
-        if movies == oldTitle:
-            movie_genres[genre][i] = newTitle
+    if genre in movie_genres:
+        if oldTitle in movie_genres[genre]:
+            for i, movies in enumerate(movie_genres[genre]):
+                if movies == oldTitle:
+                    movie_genres[genre][i] = newTitle
+            print(f"Successfully updated '{oldTitle}' to '{newTitle}'.")
+        else:
+            print(f"Error: Could not find '{oldTitle}' in '{genre}'.")
+    else:
+        print(f"Error: Genre '{genre}' does not exist.")
 
 def delete_movies(genre, title):
     if genre in movie_genres:
         if title in movie_genres[genre]:
             movie_genres[genre].remove(title)
-                 
+            print(f"Successfully deleted '{title}'.")
+        else:
+            print(f"Error: Could not find '{title}' in '{genre}'.")
+    else:
+        print(f"Error: Genre '{genre}' does not exist.")
 
-update_movies("Action", "Spiderman Brand new day", "Spiderman")
-update_movies("Musical", "Mamma Mia", "The Greatest Showman")
+# pang test lang to sa mga functions
+genre = input("Enter genre to update: ")
+old_title = input("Enter old title: ")
+new_title = input("Enter new title: ")
+update_movies(genre, old_title, new_title)
 
-print(movie_genres)
+show_Movies()
 
-delete_movies("Sci-Fi", "Star wars")
-
-print(movie_genres)
+del_genre = input("Enter genre to delete from: ")
+del_title = input("Enter movie title to delete: ")
+delete_movies(del_genre, del_title)
 
 show_Movies()
